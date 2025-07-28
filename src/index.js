2ac8dc1ee5412adb2086974e00a38ba067ab12b3
DEV_BUDDY <allishittuabdulhameed@gmail.com>
2025-07-28 15:28:07 +0100

    cors error resolve

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from 'cors'
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
+const allowedOrigin = [
+  "http://localhost:5173",
+  "https://worldnumhub.vercel.app/",
+];
 
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://worldnumhub.vercel.app/"
-    ],
-    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // if you're using cookies/auth headers
   })
 );
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/api/auth', authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, ()=> {
-  console.log(`server running on port ${PORT} `)
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT} `);
   connectDB();
-})
+});
